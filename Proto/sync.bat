cdeea7d6190c05df417d37f99ed6f10a5561ae58
msrocka <michael.srocka@gmail.com>
2021-04-22 10:11:09 +0200

    updated the proto files

--- Proto/sync.bat
+++ Proto/sync.bat
@@ -1,10 +1,19 @@
-rem @echo off
+@echo off
 
 rem Copies the *.proto files from the olca-modules project to this project
 rem assuming that this project is located next to the olca-modules project.
 
 set script_home=%~dp0
+for %%G in (%script_home%\*.proto) do (
+    del %%G
+)
+
 set mods_proto= %script_home%..\..\olca-modules\olca-proto\src\main\proto
 if exist %mods_proto% (
     xcopy /y %mods_proto% %script_home%
+)
+
+echo add the following includes:
+for %%G in (*.proto) do (
+    echo     ^<Protobuf Include="Proto\%%G" GrpcServices="Client" ProtoRoot="Proto\" /^>
 )
